15a741f08ad836668273fa83dee9e0642f726bf1
qbarefoot <quintenbarefoot@gmail.com>
2021-08-07 12:23:19 -0400

    trip folder created with models and .py files

--- trip/models.py
+++ trip/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from datetime import date
+from django.db.models.deletion import CASCADE
+from accounts .models import UserAccount
 
 
 class Trip(models.Model):
@@ -9,7 +11,7 @@ class Trip(models.Model):
     state = models.CharField(max_length=100)
     start_date = models.DateField(default=date.today)
     end_date = models.DateField(default=date.today)
-    guide = models.ForeignKey(User)
+    guide = models.ForeignKey(UserAccount, on_delete=CASCADE)
 
     def __str__(self):
         return self.trip_title
