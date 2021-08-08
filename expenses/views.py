54aa434ca8cbe113065cf6d5e5abe918e7b9cf96
smilewendy7 <smilewendy7@gmail.com>
2021-08-07 20:40:16 -0400

    update expenses view and urls

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListAPIView, 
+    RetrieveAPIView,
+    DestroyAPIView,
+    RetrieveUpdateAPIView,
+    UpdateAPIView,
+    CreateAPIView,
+    get_object_or_404
+    )
 
-# Create your views here.
+from rest_framework.permissions import(
+    AllowAny,
+    IsAuthenticatedOrReadOnly,
+    IsAuthenticated
+)
+
+from .models import Expense
+
+from .serializers import (
+    ExpenseCreateSerializer,
+    ExpenseDetailSerializer,
+    ExpenseListSerializer,
+    UpdateExpenseSerializer,
+)
+
+from .permissions import IsOwnerOrReadOnly
+
+#### Create Expense
+class ExpenseCreate(CreateAPIView):
+    queryset = Expense.objects.all()
+
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ExpenseCreateSerializer
+
+
+###  ** list answers ---- and add/create
+class ExpenseList(ListAPIView):
+    queryset = Expense.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
+    # queryset = Book.objects.all()
+    serializer_class = ExpenseListSerializer
+    ### pagination 
+
+
+
+### ** Expense details 
+class ExpenseDetail(RetrieveAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseDetailSerializer
+
+    
+### ** Expense delete   
+class ExpenseDelete(DestroyAPIView):
+    ### only author can update and delete ... 
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseCreateSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+
+### ** Expense update  
+class ExpenseUpdate(RetrieveUpdateAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = UpdateExpenseSerializer
+    permission_classes = [IsOwnerOrReadOnly]
