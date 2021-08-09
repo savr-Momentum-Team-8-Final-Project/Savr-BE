3ad76383fb566256c7b9ce97f44c6b9226fddada
qbarefoot <quintenbarefoot@gmail.com>
2021-08-09 14:14:18 -0400

    updating trip serializers added detail serializer

--- trip/serializers.py
+++ trip/serializers.py
@@ -1,7 +1,8 @@
-from expenses.models import Expense
+from expenses.serializers import NoLinkExpenseListSerializer
 from rest_framework import serializers
 from trip.models import Trip
 from rest_framework.serializers import SerializerMethodField
+from expenses.models import Expense
 
 
 
@@ -25,10 +26,38 @@ class TripListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trip
         fields ='__all__'
-    
+
     def get_guide(self,obj):
         return str(obj.guide.name)
 
-
-
     
+class TripDetailSerialzier(serializers.ModelSerializer):
+    guide = SerializerMethodField()
+    expenses = SerializerMethodField()
+    
+    class Meta:
+        model = Trip
+        fields = [
+            'id',
+            'trip_title',
+            'city',
+            'state',
+            'start_date',
+            'end_date',
+            'guide',
+            'budget',
+            'expenses',
+        ]
+
+    def get_guide(self,obj):
+        return str(obj.guide.name)
+
+    def get_expenses(self,obj):
+        e__qs = Expense.objects.filter(trip_id=obj.id)
+        expenses = NoLinkExpenseListSerializer(e__qs, many=True).data
+        return expenses
+
+
+
+
+
--- trip/urls.py
+++ trip/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', views.TripList.as_view(), name='trip_list'),
     path('create/', views.TripCreate.as_view(), name='trip_create'),
+    path('<int:pk>/', views.TripDetail.as_view(), name='trip-detail')
 ]
